13b79bf79f355010f335a7ce312d8b73ff6a869c
Avraham Raviv <56107590+AvrahamRaviv@users.noreply.github.com>
2023-03-12 12:42:25 +0200

    Add video creator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import spacy
+import cv2
 
 nlp = spacy.load("en_core_web_sm")
 
@@ -21,6 +22,8 @@ def Detect_descriptive_words(Sentence):
 
 
 if __name__ == '__main__':
+    # Part 1: Detect descriptive words from a sentence (user input)
+
     # get sentence from user
     sentence = "A cat with a hat is lying on a beach chair"
     # sentence = input("Enter a sentence: ")
@@ -29,5 +32,17 @@ if __name__ == '__main__':
     # print the sentence and the descriptive words
     print("The sentence is: ", sentence)
     print("The descriptive words are: ", descriptive_words)
-    print("The indices of the descriptive words are: ", indices_dw)
+
+    # Part 2: Generate images using diffusion models
+    images = []
+    # TODO: create more images using diffusion models
+
+    # Part 3: create a gif or video from the images
+    height, width, layers = images[0].shape
+    size = (width, height)
+    frame_rate = 2
+    out = cv2.VideoWriter(f'scene.avi', cv2.VideoWriter_fourcc(*'DIVX'), frame_rate, size)
+    for i in range(len(images)):
+        out.write(images[i])
+    out.release()
 
