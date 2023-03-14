6e9002dd937cc40c5fde62bf824238795ab8dde0
Itamar Tsayag <itamars@aivf.co>
2023-03-14 21:24:33 +0200

    --no-edit

--- src/descriptor_gif/imgs_to_gif.py
+++ src/descriptor_gif/imgs_to_gif.py
@@ -16,24 +16,30 @@ class Imgs2Gif(nn.Module):
         prompt = f"Enter a sentence to animate the descriptors"
         sentence = Prompt.ask(f":rocket: {prompt}", default=5)
         return sentence
-    
+
     def cprint(self, msg, style=None):
         self.console.print(f'{msg}', style=style)
 
-    def forward(self, imgs:List[np.ndarray], destination=None):
-        assert destination is not None, self.cprint("Please insert valid sestination for gif", style="bold red on white")
-        assert len(imgs) != 0, self.cprint("Image list is empty!", style="bold red on white")
+    def forward(self, imgs: List[np.ndarray], destination=None):
+        assert destination is not None, self.cprint(
+            "Please insert valid sestination for gif", style="bold red on white")
+        assert len(imgs) != 0, self.cprint(
+            "Image list is empty!", style="bold red on white")
         height, width, _ = imgs[0].shape
         size = (width, height)
         frame_rate = 1
-        out = cv2.VideoWriter(f'{destination}', cv2.VideoWriter_fourcc(*'DIVX'), frame_rate, size)
+        out = cv2.VideoWriter(
+            f'{destination}', cv2.VideoWriter_fourcc(*'DIVX'), frame_rate, size)
         for i in range(len(imgs)):
             out.write(imgs[i])
         out.release()
-        self.cprint(msg=f'Succesfully saved gif to {destination}', style='green')
+        self.cprint(
+            msg=f'Succesfully saved gif to {destination}', style='green')
 
 
 if __name__ == '__main__':
     img2gif = Imgs2Gif()
-    a = np.random.randint(low=0, high=255, size=(20, 224, 224, 3)).astype(np.uint8)
-    img2gif.forward(a, destination='/Users/itamar/Git/SweepedDescriptors/test.avi')
+    a = np.random.randint(low=0, high=255, size=(
+        20, 224, 224, 3)).astype(np.uint8)
+    img2gif.forward(
+        a, destination='/Users/itamar/Git/SweepedDescriptors/test.avi')
--- src/descriptor_gif/prompt_to_image.py
+++ src/descriptor_gif/prompt_to_image.py
@@ -1,13 +1,41 @@
+import abc
+from typing import List
+
 import numpy as np
+import torch
+import torch.nn.functional as nnf
+from diffusers import DiffusionPipeline
+from IPython.display import display
+from PIL import Image
+from prompt_to_prompt import ptp_utils, seq_aligner
 from rich.console import Console
 from rich.prompt import Prompt
 from torch import nn
 
 
 class Prompt2Img(nn.Module):
-    def __init__(self) -> None:
-        self.console = Console()
+    def __init__(self,
+                 model_id="CompVis/ldm-text2im-large-256",
+                 num_diffusion_steps=50,
+                 guidance_scale=5.,
+                 max_num_workds=77,
+                 ) -> None:
+
+        self.model_id = model_id
+        self.num_diffusion_steps = num_diffusion_steps
+        self.guidance_scale = guidance_scale
+        self.max_num_workds = max_num_workds
+        self.setup()
+
         super().__init__()
+
+    def setup(self):
+        self.ldm = DiffusionPipeline.from_pretrained(
+            self.model_id).to(self.device)
+        self.tokenizer = self.ldm.tokenizer
+        self.device = torch.device(
+            'cuda:0') if torch.cuda.is_available() else torch.device('cpu')
+        self.console = Console()
 
     def prompt_user(self):
         prompt = f"Enter a sentence to animate the descriptors"
@@ -17,7 +45,8 @@ class Prompt2Img(nn.Module):
     def cprint(self, msg, style=None):
         self.console.print(f'{msg}', style=style)
 
-    # def forward(self, prompt:str, descriptors: List[str]):
+    def forward(self, prompt: str, descriptors: List[str]):
+        pass
     #     prompts = ["A photo of a tree branch at blossom"] * 4
     #     equalizer = get_equalizer(prompts[0], word_select=("blossom",), values=(.5, .0, -.5))
     #     controller = AttentionReweight(prompts, NUM_DIFFUSION_STEPS, cross_replace_steps=1., self_replace_steps=.2, equalizer=equalizer)
@@ -25,8 +54,6 @@ class Prompt2Img(nn.Module):
 
 
 if __name__ == '__main__':
-    img2gif = Prompt2Img()
-    a = np.random.randint(low=0, high=255, size=(
-        20, 224, 224, 3)).astype(np.uint8)
-    img2gif.forward(
-        a, destination='/Users/itamar/Git/SweepedDescriptors/test.avi')
+    p2gif = Prompt2Img()
+    prompt = "The cake is red"
+    p2gif.forward(prompt, destination='/Users/itamar/Git/SweepedDescriptors/test.avi')
