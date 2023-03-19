1efbb9a6d400f169c3b1b1215a2f5108f6dc7a3f
Itamar Tsayag <itamars@aivf.co>
2023-03-19 11:55:52 +0200

    --no-edit

--- src/descriptor_gif/prompt_to_image.py
+++ src/descriptor_gif/prompt_to_image.py
@@ -1,12 +1,7 @@
-from typing import List, Optional, Tuple, Union
 
-import numpy as np
 import torch
-import torch.nn.functional as nnf
 from diffusers import DiffusionPipeline
-from IPython.display import display
-from PIL import Image
-from prompt_to_prompt import ptp_functional, ptp_utils, seq_aligner
+from prompt_to_prompt import ptp_functional, ptp_utils
 from rich.console import Console
 from rich.prompt import Prompt
 from torch import nn
@@ -18,7 +13,7 @@ class Prompt2Img(nn.Module):
                  num_diffusion_steps=50,
                  guidance_scale=5.,
                  max_num_workds=77,
-                 image_size = [256, 256],
+                 image_size=[256, 256],
                  ) -> None:
 
         self.model_id = model_id
@@ -46,19 +41,14 @@ class Prompt2Img(nn.Module):
 
     def cprint(self, msg, style=None):
         self.console.print(f'{msg}', style=style)
-        
+
     def get_baseline_latent_init(self, prompts):
         height, width = self.image_size
         latent, latents = ptp_utils.init_latent(latent=None, model=self.ldm,
-                                      height=height, width=width, generator=None, batch_size=len(prompts))
-        
+                                                height=height, width=width, generator=None, batch_size=len(prompts))
+
         self.x_t = latent
         return latent, latents
-
-    # def init_latent(self, prompt):
-    #     ptp_utils.init_latent(latent=self.ldm.v, model=self.ldm, height, width, generator, batch_size)
-    #     images, x_t = ptp_functional.run_and_display(prompts, controller, run_baseline=False, generator=g_cpu)
-
 
     def forward(self, prompt: str, descriptor: str, clip_len=4):
         # get baseline latent using the first prompt
@@ -70,14 +60,17 @@ class Prompt2Img(nn.Module):
         # update the latent
         # interpolate the keyframes to a video
         # save all images to a video
-        
-        prompts = [prompt] * clip_len        
+
+        prompts = [prompt] * clip_len
         latent, latents = self.get_baseline_latent_init(prompt)
-        
-        equalizer = ptp_functional.get_equalizer(prompts[0], word_select=(descriptor), values=(.5, .0, -.5), tokenizer=self.ldm.tokenizer)
-        controller = ptp_functional.AttentionReweight(prompts, self.num_diffusion_steps, cross_replace_steps=1., self_replace_steps=.2, equalizer=equalizer)
+
+        equalizer = ptp_functional.get_equalizer(prompts[0], word_select=(
+            descriptor), values=(.5, .0, -.5), tokenizer=self.ldm.tokenizer)
+        controller = ptp_functional.AttentionReweight(
+            prompts, self.num_diffusion_steps, cross_replace_steps=1., self_replace_steps=.2, equalizer=equalizer)
         x_t = latent
-        images, x_t = ptp_utils.text2image_ldm(self.ldm, prompts, controller, latent=latent, num_inference_steps=self.num_diffusion_steps, guidance_scale=self.guidance_scale, generator=None)
+        images, x_t = ptp_utils.text2image_ldm(self.ldm, prompts, controller, latent=latent,
+                                               num_inference_steps=self.num_diffusion_steps, guidance_scale=self.guidance_scale, generator=None)
         return images
 
 
