901aa2f4fdf220a4ec5dc1e5f300af40ff6d0421
Itamar Tsayag <itamars@aivf.co>
2023-03-19 12:20:08 +0200

    --no-edit

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
+echo "Start installation"
+
 pip install -r requirements.txt
 
 python -m spacy download en_core_web_sm
+
+echo "Start Downloading pretrained_models"
+
+PRETRAINED_DIR='pretrained_models'
+
+mkdir -p $PRETRAINED_DIR
+
+wget --no-clobber -P $PRETRAINED_DIR https://www.dropbox.com/sh/hrewbpedd2cgdp3/AADbEivu0-CKDQcHtKdMNJPJa?dl=0
+
+echo "Done Downloading pretrained_models"
---
+++ src/descriptor_gif/interpolate_keyframes_to_vid.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import cv2
+import numpy as np
+from rich.console import Console
+from rich.prompt import Prompt
+from torch import nn
+
+
+class InterpolateKeyframes(nn.Module):
+    def __init__(self, video_frames=300, method='firnet') -> None:
+        self.console = Console()
+        self.video_frames = video_frames
+        self.method = method
+        super().__init__()
+        
+    def setup(self):
+        
+
+    def prompt_user(self):
+        prompt = f"Enter a sentence to animate the descriptors"
+        sentence = Prompt.ask(f":rocket: {prompt}", default=5)
+        return sentence
+
+    def cprint(self, msg, style=None):
+        self.console.print(f'{msg}', style=style)
+
+    def forward_firnet(self, keyframes: List[np.ndarray]):
+        self.cprint(f'Interpolating the {len(keyframes)} keyframes')
+        
+        
+
+
+if __name__ == '__main__':
+    interpolator = InterpolateKeyframes()
+    a = np.random.randint(low=0, high=255, size=(224, 224, 3)).astype(np.uint8)
+    b = np.random.randint(low=0, high=255, size=(224, 224, 3)).astype(np.uint8)
+
+    imgs = interpolator.forward_firnet([a, b])
